729a8f6853548cab004a4d22dcfcab47f47440f1
TechWithAyaan <techwithayaanfaheem@gmail.com>
2025-08-06 11:26:07 -0700

    header complete

---
+++ app.js
@@ -0,0 +1,24 @@
+function op(){
+    document.getElementsByClassName("search-desk-icon")
+    [0].style.display = ("none")
+    document.getElementsByClassName("search")
+    [0].style.display = ("none")
+    document.getElementsByClassName("open")
+    [0].style.display = ("none")
+    document.getElementsByClassName("close")
+    [0].style.display = ("inline")
+    document.getElementsByClassName("sidebar")
+    [0].style.display = ("inline")
+}
+function cl(){
+        document.getElementsByClassName("search-desk-icon")
+    [0].style.display = ("inline")
+        document.getElementsByClassName("search")
+    [0].style.display = ("inline")
+    document.getElementsByClassName("open")
+    [0].style.display = ("inline")
+    document.getElementsByClassName("close")
+    [0].style.display = ("none")
+    document.getElementsByClassName("sidebar")
+    [0].style.display = ("none")
+}
